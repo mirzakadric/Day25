d0efe88dc4e616bca607298e6e9d79608e494d7a
Mirza Kadric <kadric.mirza@hotmail.com>
2021-04-01 14:44:10 +0200

    Making minor changes and Adding ex02

--- ex02/src/GetRequestAsyncAwait.js
+++ ex02/src/GetRequestAsyncAwait.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-class GetRequest extends Component {
+class GetRequestAsyncAwait extends Component {
     constructor(props){
         super(props);
         this.state = {
@@ -11,17 +11,17 @@ class GetRequest extends Component {
         const {totalReactPackages} = this.state;
         return(
             <div className="card text-center m-3">
-                <h5 className="card-header">Simple GET request</h5>
+                <h5 className="card-header">GET Request with Async/Await</h5>
                 <div className="card-body">Total packages: {totalReactPackages}</div>
             </div>
         );
         
     }
     componentDidMount() {
-        fetch('https://api.npms.io/v2/search?q=react')
-        .then(response => response.json())
-        .then(data => this.setState({totalReactPackages: data.total}));
+        const response = await fetch('https://api.npms.io/v2/search?q=react');
+        const data = response.json();
+        this.setState({totalReactPackages: data.total});
     }
 }
 
-export default GetRequest;
+export default GetRequestAsyncAwait;
